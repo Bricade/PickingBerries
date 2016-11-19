3a57b86c099dcc4e2a23c8c53af49c3741bfe1ef
Sug4rsh0ts <shuttleworth.brianna@gmail.com>
2016-11-19 11:39:50 -0700

    Test
    Testing

---
+++ Assets/pickup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pickup : MonoBehaviour {
+
+    public GameObject inventoryPanel;
+    public GameObject[] inventoryIcons;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //look for existing icon
+        foreach(Transform child in inventoryPanel.transform)
+        {
+            //if already in inventory
+            if(child.gameObject.tag==collision.gameObject.tag)
+            {
+                string c = child.Find("Text").GetComponent<Text>().text;
+                int tcount = System.Int32.Parse(c) + 1;
+                child.Find("Text").GetComponent<Text>().text = "" + tcount;
+                return;
+            }
+        }
+
+        GameObject i;
+        if(collision.gameObject.tag =="red")
+        {
+            i = Instantiate(inventoryIcons[0]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+        else if(collision.gameObject.tag=="blue")
+        {
+            i = Instantiate(inventoryIcons[1]);
+            i.transform.SetParent(inventoryPanel.transform);
+        }
+
+    }
+
+}
